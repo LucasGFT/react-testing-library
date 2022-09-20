2db24563da165df19943aa74b68e6d5861f721d3
Lucas Gomes Filgueiras Tavares <lucasgft200303@gmail.com>
2022-09-20 15:47:59 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+import { FavoritePokemons } from '../pages';
+
+test('É exibido na tela a mensagem No favorite pokemon found', () => {
+  renderWithRouter(<FavoritePokemons />);
+  const NoFavorite = screen.getByText('No favorite pokemon found');
+  expect(NoFavorite).toBeInTheDocument();
+});
