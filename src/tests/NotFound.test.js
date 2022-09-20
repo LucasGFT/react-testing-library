1eb274253f8b205f9d462f958a43f5ed0bef3efd
Lucas Gomes Filgueiras Tavares <lucasgft200303@gmail.com>
2022-09-20 15:53:01 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+test('É exibido na tela um h2 com o texto Page requested not found', () => {
+  renderWithRouter(<NotFound />);
+  const hDois = screen.getByRole('heading', { name: 'Page requested not found' });
+  expect(hDois).toBeInTheDocument();
+});
+test('O atributo src da imagem é', () => {
+  renderWithRouter(<NotFound />);
+  const srcImg = screen.getByRole('img');
+  expect(srcImg.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
