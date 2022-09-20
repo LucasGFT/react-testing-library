657675fc1226dc78a4ccadeb8c92ce1ba6e52d7e
Lucas Gomes Filgueiras Tavares <lucasgft200303@gmail.com>
2022-09-20 15:30:35 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import About from '../pages/About';
+
+test('É exibido na tela um h2 com texto About Pokédex', () => {
+  renderWithRouter(<About />);
+  const hDois = screen.getByRole('heading', { name: 'About Pokédex' });
+  expect(hDois).toBeInTheDocument();
+});
+
+test('O atributo src da imagem é', () => {
+  renderWithRouter(<About />);
+  const srcImg = screen.getByRole('img');
+  expect(srcImg.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
