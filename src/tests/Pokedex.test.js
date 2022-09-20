f969ec4b813c2ae345ca5e59072968bb43bd16ce
Lucas Gomes Filgueiras Tavares <lucasgft200303@gmail.com>
2022-09-20 16:32:57 -0300

    Requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+test('Os botões de filtragem por tipo possuem o nome correto', () => {
+  renderWithRouter(<App />);
+
+  const a = screen.getByRole('button', { name: 'All' });
+  expect(a).toHaveTextContent('All');
+  const types = ['Electric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon'];
+  const textButton = screen.getAllByTestId('pokemon-type-button');
+  textButton.map((element, index) => expect(element.textContent).toBe(types[index]));
+});
+
+test('É possível clicar no botão de filtragem All', () => {
+  renderWithRouter(<App />);
+
+  const a = screen.getByRole('button', { name: 'All' });
+  expect(a).toBeInTheDocument();
+  userEvent.click(a);
+});
